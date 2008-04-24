322c87492a2f7f9c641ae9d729c1a4ae581c2864
Gary Poster <gary@modernsongs.com>
2008-04-24 01:10:54 +0000

    license on setup.py, why not

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
+##############################################################################
+#
+# Copyright (c) 2006-2008 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
 from setuptools import setup, find_packages
-
 
 long_description = (
     open('src/zc/relation/README.txt').read() + "\n" +
